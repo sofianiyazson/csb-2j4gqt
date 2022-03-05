750387c1005c9723bd023a6d0ffa9c3c844ff993
Code Sandbox <hello@codesandbox.io>
2022-03-05 15:11:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.24a3e8633cfc59a62521f782e89d56f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ec997874a23703e2c4c341b9d9217b",
+    "url": "https://sofianiyazson.github.io/csb-2j4gqt/index.html"
+  },
+  {
+    "revision": "b52aadf0f3b9303d7360",
+    "url": "https://sofianiyazson.github.io/csb-2j4gqt/static/css/main.06bf2c79.chunk.css"
+  },
+  {
+    "revision": "a0513bf2ea8e6e83467c",
+    "url": "https://sofianiyazson.github.io/csb-2j4gqt/static/js/2.8bc12123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://sofianiyazson.github.io/csb-2j4gqt/static/js/2.8bc12123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52aadf0f3b9303d7360",
+    "url": "https://sofianiyazson.github.io/csb-2j4gqt/static/js/main.3fecafbc.chunk.js"
+  },
+  {
+    "revision": "a4f772495fce11d9d636",
+    "url": "https://sofianiyazson.github.io/csb-2j4gqt/static/js/runtime-main.f84635b6.js"
+  }
+]);
